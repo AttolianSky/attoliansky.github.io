d8131950ac3a759f2732454b67f02c79cf5c4f13
AttolianSky <attoliansky@gmail.com>
2021-04-29 15:19:26 +0800

    CABot R1

--- assets/js/main.js
+++ assets/js/main.js
@@ -214,4 +214,10 @@
     }
   });
 
+	const dfMessenger = document.querySelector('df-messenger');
+	dfMessenger.addEventListener('event-type', function (event) {
+	  // Handle event
+	  ...
+	});
+
 })()
